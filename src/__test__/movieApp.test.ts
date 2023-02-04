8733ad7ca65009b9293628708112c5d0e45020a7
Grapensparr <113124538+Grapensparr@users.noreply.github.com>
2023-02-04 20:06:24 +0100

    Tests for creating HTML elements and the noMessage

--- src/__test__/movieApp.test.ts
+++ src/__test__/movieApp.test.ts
@@ -3,6 +3,11 @@
 */
 
 import * as movieApp from '../ts/movieApp'
+import { mockListOfMovies } from '../ts/services/__mocks__/movieservicemock';
+import axios from 'axios';
+
+jest.mock('axios');
+const mockAxios = axios as jest.Mocked<typeof axios>;
 
 beforeEach(() => {
     document.body.innerHTML = '';
@@ -40,38 +45,89 @@ describe('Functions related to handleSubmit', () => {
             <div id="movie-container"></div>
         `;
 
-        (document.getElementById('searchText') as HTMLInputElement).value = 'test';
+        (document.getElementById('searchText') as HTMLInputElement).value = 'The Departed';
         const spyOnCreateHtml = jest.spyOn(movieApp, 'createHtml').mockReturnValue();
 
-        //Act
-        await movieApp.handleSubmit();
+        mockAxios.get.mockResolvedValue({ data: { Search: mockListOfMovies } });
 
-        //Assert
+        //act
+        await movieApp.handleSubmit();
+    
+        //assert
         expect(spyOnCreateHtml).toHaveBeenCalled();
         spyOnCreateHtml.mockRestore();
     });
 
-    test('Should call displayNoResults again', async () => {
+    test('Should call displayNoResults', async () => {
         //Arrange
         document.body.innerHTML = `
             <input type="text" id="searchText" placeholder="Skriv titel här" />
             <div id="movie-container"></div>
         `;
 
-        (document.getElementById('searchText') as HTMLInputElement).value = '';
+        (document.getElementById('searchText') as HTMLInputElement).value = 'Display error';
         const spyOnDisplayNoResults = jest.spyOn(movieApp, 'displayNoResult').mockReturnValue();
 
-        //Act
-        await movieApp.handleSubmit();
+        mockAxios.get.mockRejectedValue({ data: { Search: mockListOfMovies } });
 
-        //Assert
+        //act
+        await movieApp.handleSubmit();
+    
+        //assert
         expect(spyOnDisplayNoResults).toHaveBeenCalled();
         spyOnDisplayNoResults.mockRestore();
-    })
+    });
+
+    test('Should call displayNoResults on catch path', async () => {
+        //Arrange
+        document.body.innerHTML = `
+            <input type="text" id="searchText" placeholder="Skriv titel här" />
+            <div id="movie-container"></div>
+        `;
+
+        (document.getElementById('searchText') as HTMLInputElement).value = 'Display error';
+        const spyOnDisplayNoResult = jest.spyOn(movieApp, 'displayNoResult').mockReturnValue();
+
+        mockAxios.get.mockImplementation(() => {
+            throw new Error('Test Error');
+        });
+        
+        // Act
+        await movieApp.handleSubmit();
+        
+        // Assert
+        expect(spyOnDisplayNoResult).toHaveBeenCalled();
+        spyOnDisplayNoResult.mockRestore();
+    });
 })
 
-        //Arrange
+test('Should create HTML elements', () => {
+    //Arrange
+    document.body.innerHTML = `
+        <div id="movie-container"></div>
+    `;
 
-        //Act
+    const movieContainer = document.getElementById('movie-container') as HTMLDivElement;
 
-        //Assert
+    //Act
+    movieApp.createHtml(mockListOfMovies, movieContainer);
+
+    //Assert
+    const movies = document.querySelectorAll('.movie');
+    expect(movies.length).toEqual(mockListOfMovies.length);
+});
+
+test('Should display the noMessage', () => {
+    //Arrange
+    document.body.innerHTML = `
+        <div id="movie-container"></div>
+    `;
+
+    const movieContainer = document.getElementById('movie-container') as HTMLDivElement;
+
+    //Act
+    movieApp.displayNoResult(movieContainer);
+
+    //Assert
+    expect(movieContainer.textContent).toBe('Inga sökresultat att visa');
+});
