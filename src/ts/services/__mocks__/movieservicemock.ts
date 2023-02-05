9799a1026750de28db33f80c8f741b74e68a1844
Grapensparr <113124538+Grapensparr@users.noreply.github.com>
2023-02-05 18:10:15 +0100

    Adjustment to the mock list

--- src/ts/services/__mocks__/movieservicemock.ts
+++ src/ts/services/__mocks__/movieservicemock.ts
@@ -6,47 +6,41 @@ export let mockListOfMovies: IMovie[] = [
         Year: '2010',
         imdbID: 'tt1403981',
         Type: 'movie',
-        Poster:
-            'https://m.media-amazon.com/images/M/MV5BOTA4MTg1ODkwNF5BMl5BanBnXkFtZTcwMzE5ODAxMw@@._V1_SX300.jpg'
+        Poster: 'https://m.media-amazon.com/images/M/MV5BOTA4MTg1ODkwNF5BMl5BanBnXkFtZTcwMzE5ODAxMw@@._V1_SX300.jpg'
     },
     {
         Title: 'The Green Mile',
         Year: '1999',
         imdbID: 'tt0120689',
         Type: 'movie',
-        Poster:
-            'https://m.media-amazon.com/images/M/MV5BMTUxMzQyNjA5MF5BMl5BanBnXkFtZTYwOTU2NTY3._V1_SX300.jpg'
+        Poster: 'https://m.media-amazon.com/images/M/MV5BMTUxMzQyNjA5MF5BMl5BanBnXkFtZTYwOTU2NTY3._V1_SX300.jpg'
     },
     {
         Title: 'The Departed',
         Year: '2006',
         imdbID: 'tt0407887',
         Type: 'movie',
-        Poster:
-            'https://m.media-amazon.com/images/M/MV5BMTI1MTY2OTIxNV5BMl5BanBnXkFtZTYwNjQ4NjY3._V1_SX300.jpg'
+        Poster: 'https://m.media-amazon.com/images/M/MV5BMTI1MTY2OTIxNV5BMl5BanBnXkFtZTYwNjQ4NjY3._V1_SX300.jpg'
     },
     {
         Title: 'Anaconda',
         Year: '1997',
         imdbID: 'tt0118615',
         Type: 'movie',
-        Poster:
-            'https://m.media-amazon.com/images/M/MV5BZDc4ODcyNWMtMzI2Zi00YzcwLWE4ZTUtYWM3OWI1MTgwYTc1XkEyXkFqcGdeQXVyNzc5MjA3OA@@._V1_SX300.jpg'
+        Poster: 'https://m.media-amazon.com/images/M/MV5BZDc4ODcyNWMtMzI2Zi00YzcwLWE4ZTUtYWM3OWI1MTgwYTc1XkEyXkFqcGdeQXVyNzc5MjA3OA@@._V1_SX300.jpg'
     },
     {
         Title: 'Remember Me',
         Year: '2010',
         imdbID: 'tt1403981',
         Type: 'movie',
-        Poster:
-            'https://m.media-amazon.com/images/M/MV5BOTA4MTg1ODkwNF5BMl5BanBnXkFtZTcwMzE5ODAxMw@@._V1_SX300.jpg'
+        Poster: 'https://m.media-amazon.com/images/M/MV5BOTA4MTg1ODkwNF5BMl5BanBnXkFtZTcwMzE5ODAxMw@@._V1_SX300.jpg'
     },
     {
         Title: 'Shutter Island',
         Year: '2010',
         imdbID: 'tt1130884',
         Type: 'movie',
-        Poster:
-            'https://m.media-amazon.com/images/M/MV5BYzhiNDkyNzktNTZmYS00ZTBkLTk2MDAtM2U0YjU1MzgxZjgzXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg'
+        Poster: 'https://m.media-amazon.com/images/M/MV5BYzhiNDkyNzktNTZmYS00ZTBkLTk2MDAtM2U0YjU1MzgxZjgzXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg'
     }
 ];
